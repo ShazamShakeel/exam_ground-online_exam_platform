33dc926a008c3da868caf5127f15a0ac8cb09749
ShazamShakeel <Shazammeharofficial@gmail.com>
2023-03-17 02:22:23 +0500

    feat: added add course functionality

--- src/utils/httpRequest/axiosInstance.js
+++ src/utils/httpRequest/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  // baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: "http://localhost:3002/v1",
 });
 
 axiosInstance.interceptors.request.use(
