69ddca6c254645f1e859ebcef37aa2f8962169c2
ShazamShakeel <Shazammeharofficial@gmail.com>
2023-04-09 23:09:20 +0500

    feat: announcements

--- src/pages/Announcements.jsx
+++ src/pages/Announcements.jsx
@@ -3,95 +3,67 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import { Button, Divider, IconButton, Stack, Typography } from "@mui/material";
 import CustomDataGrid from "components/CustomDataGrid";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import axiosInstance from "utils/httpRequest/axiosInstance";
+const localizedFormat = require("dayjs/plugin/localizedFormat");
+dayjs.extend(localizedFormat);
 
 function Announcements() {
   const navigate = useNavigate();
   const userRole = useSelector((state) => state.auth.userRole);
-  const loading = useSelector(
-    (state) => state?.announcements?.loading ?? false
-  );
-  const totalPages = 1;
-  const announcements = [
-    {
-      id: 1,
-      title: "System Maintenance",
-      description:
-        "Our system will be undergoing maintenance on March 1st from 8:00 PM to 10:00 PM. During this time, the system will be temporarily unavailable.",
-      date: "2023-02-25",
-    },
-    {
-      id: 2,
-      title: "New Exam Added",
-      description:
-        "We have added a new exam on 'Introduction to Computer Science'. Please check the 'Exams' tab for more details.",
-      date: "2023-02-18",
-    },
-    {
-      id: 3,
-      title: "Exam Schedule Change",
-      description:
-        "The exam on 'Database Management Systems' previously scheduled for March 5th has been rescheduled to March 12th. Please check the 'Exams' tab for more details.",
-      date: "2023-02-16",
-    },
-    {
-      id: 4,
-      title: "Exam Results Available",
-      description:
-        "The results for the 'Operating Systems' exam are now available. Please check the 'Results' tab for more details.",
-      date: "2023-02-14",
-    },
-    {
-      id: 5,
-      title: "System Upgrade",
-      description:
-        "We are upgrading our system to improve its performance and stability. During this time, the system may be temporarily unavailable. We apologize for any inconvenience.",
-      date: "2023-02-11",
-    },
-    {
-      id: 6,
-      title: "New Feature Added",
-      description:
-        "We have added a new feature that allows you to bookmark questions during an exam. Please check the 'Features' tab for more details.",
-      date: "2023-02-09",
-    },
-    {
-      id: 7,
-      title: "Exam Registration Open",
-      description:
-        "Registration for the 'Web Development' exam is now open. Please check the 'Exams' tab for more details.",
-      date: "2023-02-06",
-    },
-    {
-      id: 8,
-      title: "Exam Cancellation",
-      description:
-        "The exam on 'Computer Networks' scheduled for March 8th has been cancelled. We apologize for any inconvenience.",
-      date: "2023-02-04",
-    },
-    {
-      id: 9,
-      title: "New Exam Added",
-      description:
-        "We have added a new exam on 'Data Structures and Algorithms'. Please check the 'Exams' tab for more details.",
-      date: "2023-02-01",
-    },
-    {
-      id: 10,
-      title: "System Downtime",
-      description:
-        "Our system will be down for maintenance on February 28th from 10:00 PM to 12:00 AM. During this time, the system will be unavailable.",
-      date: "2023-01-28",
-    },
-  ];
+  const [loading, setLoading] = useState(true);
+  const [announcements, setAnnouncements] = useState([]);
+
+  const getAnnouncements = () => {
+    setLoading(true);
+    axiosInstance
+      .get("/announcement", {
+        params: {
+          populate: "course",
+          sortBy: "-updatedAt",
+        },
+      })
+      .then((res) => {
+        setAnnouncements(res.data.results);
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message ?? "Something went wrong");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const handleDelete = (id) => {
+    setLoading(true);
+    axiosInstance
+      .delete("/announcement/" + id)
+      .then(() => {
+        toast.success("Announcement deleted successfully");
+        getAnnouncements();
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message ?? "Something went wrong");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getAnnouncements();
+  }, []);
 
   const columns = [
     {
       field: "title",
       headerName: "Title",
       minWidth: 150,
-      flex: 0.5,
+      flex: 0.75,
       renderCell: (params) => (
         <Typography variant="body1">{params?.row?.title}</Typography>
       ),
@@ -100,7 +72,7 @@ function Announcements() {
       field: "description",
       headerName: "Description",
       minWidth: 250,
-      flex: 2,
+      flex: 1,
       renderCell: (params) => (
         <Typography
           variant="body2"
@@ -120,12 +92,23 @@ function Announcements() {
       ),
     },
     {
+      field: "course",
+      headerName: "Course",
+      minWidth: 150,
+      flex: 0.5,
+      renderCell: (params) => (
+        <Typography variant="body2">{`${params?.row?.course?.code}: ${params?.row?.course?.title}`}</Typography>
+      ),
+    },
+    {
       field: "date",
       headerName: "Date",
       minWidth: 100,
       flex: 0.25,
       renderCell: (params) => (
-        <Typography variant="body2">{params?.row?.date}</Typography>
+        <Typography variant="body2">
+          {dayjs(params?.row?.date).format("LL")}
+        </Typography>
       ),
     },
     {
@@ -143,17 +126,17 @@ function Announcements() {
           >
             <EditIcon />
           </IconButton>
-          <IconButton variant="contained" size="small">
+          <IconButton
+            variant="contained"
+            size="small"
+            onClick={() => handleDelete(params.row.id)}
+          >
             <DeleteIcon />
           </IconButton>
         </Stack>
       ),
     },
   ];
-
-  const handlePagination = (_, page) => {
-    console.log("page", page);
-  };
 
   return (
     <>
@@ -183,8 +166,6 @@ function Announcements() {
         loading={loading}
         rows={announcements}
         columns={columns}
-        totalPages={totalPages}
-        handlePagination={handlePagination}
       />
     </>
   );
